084cc39e0d9c9c5a3c2fe05396a182533229336d
voleti-sruthi <sruthi.lakshmi@m2pfintech.com>
2023-03-24 22:18:54 +0530

    Add. Included the logic to print if a number is Topper number or not

--- src/ProbableTopperNumberOrNot.java
+++ src/ProbableTopperNumberOrNot.java
@@ -1,5 +1,26 @@
+import java.util.Scanner;
+
 public class ProbableTopperNumberOrNot {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter the registration number");
+        String registrationNumber = scanner.nextLine();
+        int evenCount = 0;
+        int oddCount = 0;
+        for (int iterator = 0; iterator < registrationNumber.length(); iterator++) {
+            int digit = registrationNumber.charAt(iterator) - '0';
+            if (digit % 2 == 0) {
+                evenCount = evenCount + digit;
+            } else {
+                oddCount = oddCount + digit;
+            }
+        }
+        if (evenCount == oddCount) {
+            System.out.println("yes");
+        } else {
+            System.out.println("no");
+        }
+
     }
+
 }
